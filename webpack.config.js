70930263c245c5c6b626c8f3ab5dcd9a5d7e93fa
Sérgio Gomes <mail@sgomes.com>
2019-01-25 17:42:56 +0000

    Build Calypso for multiple targets: fallback and evergreen.
    Thi PR reworks the build infrastructure to support two
    separate builds, and to serve the correct one to each
    user, based on user agent string.
    
    Squashed commits:
    
    Unify browserslist usage based on package.json.
    
    This commit:
    - Removes duplicate browserslist definition in Babel config.
    - Uses the browserslist definition to configure Terser ES6 and
      browser-specific settings.
    - Prepares package.json for multiple browserslists, to enable
      builds for multiple targets.
    - Defines the BROWSERSLIST_ENV environment variable in
      `webpack.config.js`, which is used by all browserslist-aware
      tools to pick the right configuration. This will enable builds
      for multiple targets to be controlled by WebPack.
    
    Hardcode IE8 support to false
    
    Add documentation for chooseTerserEcmaVersion function.
    
    Make Calypso generate two builds: evergreen and fallback.
    
    We currently have a single, fallback build, which includes support
    for every supported browser. This results in a large amount of
    polyfills and syntactic transformations which bloat the bundle and
    increase browser download and execution times.
    
    This PR aims to generate a separate evergreen build in production,
    and serve that build instead to evergreen browsers, by doing
    user agent detection. Other browsers will still work, since they
    have the fallback bundle, but known modern browsers will get the
    benefit of a smaller bundle.
    
    Remove assignment from noModule property.
    
    Write fallback build files to public/fallback.
    
    Fix dev builds and make them use 'evergreen' settings.
    
    Rename isEvergreen prop to addEvergreenCheck
    
    Only run AssetsWriter for client builds.
    
    Ensure non-client builds use defaults instead of evergreen.
    
    Attempt to fix desktop build.
    
    Add comment to babel.config.js
    
    Try to remove request dependency without breaking desktop build.
    
    Remove unreleased Safari versions from browserslist.
    
    Make fallback and evergreen builds happen in parallel.
    
    Fix arrow functions being used as constructors
    
    Ensure that BROWSERSLIST_ENV is always defined in WebPack
    
    Remove NODE_ARGS from build-client-both

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,15 @@ const shouldEmitStatsWithReasons = process.env.EMIT_STATS === 'withreasons';
 const shouldCheckForCycles = process.env.CHECK_CYCLES === 'true';
 const codeSplit = config.isEnabled( 'code-splitting' );
 const isCalypsoClient = process.env.CALYPSO_CLIENT === 'true';
+const isDesktop = calypsoEnv === 'desktop';
+
+const defaultBrowserslistEnv = isCalypsoClient || isDesktop ? 'evergreen' : 'defaults';
+const browserslistEnv = process.env.BROWSERSLIST_ENV || defaultBrowserslistEnv;
+const extraPath = browserslistEnv === 'defaults' ? 'fallback' : browserslistEnv;
+
+if ( ! process.env.BROWSERSLIST_ENV ) {
+	process.env.BROWSERSLIST_ENV = browserslistEnv;
+}
 
 /*
  * Create reporter for ProgressPlugin (used with EMIT_STATS)
@@ -142,8 +151,8 @@ function getWebpackConfig( {
 		mode: isDevelopment ? 'development' : 'production',
 		devtool: process.env.SOURCEMAP || ( isDevelopment ? '#eval' : false ),
 		output: {
-			path: path.join( __dirname, 'public' ),
-			publicPath: '/calypso/',
+			path: path.join( __dirname, 'public', extraPath ),
+			publicPath: `/calypso/${ extraPath }/`,
 			filename: '[name].[chunkhash].min.js', // prefer the chunkhash, which depends on the chunk, not the entire build
 			chunkFilename: '[name].[chunkhash].min.js', // ditto
 			devtoolModuleFilenameTemplate: 'app:///[resource-path]',
@@ -166,10 +175,8 @@ function getWebpackConfig( {
 				parallel: workerCount,
 				sourceMap: Boolean( process.env.SOURCEMAP ),
 				terserOptions: {
-					ecma: 5,
-					safari10: true,
 					mangle: calypsoEnv !== 'desktop',
-				},
+				}
 			} ),
 		},
 		module: {
@@ -180,7 +187,7 @@ function getWebpackConfig( {
 				TranspileConfig.loader( {
 					workerCount,
 					configFile: path.join( __dirname, 'babel.config.js' ),
-					cacheDirectory: path.join( __dirname, 'build', '.babel-client-cache' ),
+					cacheDirectory: path.join( __dirname, 'build', '.babel-client-cache', extraPath ),
 					cacheIdentifier,
 					exclude: /node_modules\//,
 				} ),
@@ -259,10 +266,15 @@ function getWebpackConfig( {
 			new webpack.IgnorePlugin( /^props$/ ),
 			isCalypsoClient && new webpack.IgnorePlugin( /^\.\/locale$/, /moment$/ ),
 			...SassConfig.plugins( { cssFilename, minify: ! isDevelopment } ),
-			new AssetsWriter( {
-				filename: 'assets.json',
-				path: path.join( __dirname, 'server', 'bundler' ),
-			} ),
+			isCalypsoClient &&
+				new AssetsWriter( {
+					filename:
+						browserslistEnv === 'defaults'
+							? 'assets-fallback.json'
+							: `assets-${ browserslistEnv }.json`,
+					path: path.join( __dirname, 'server', 'bundler' ),
+					assetExtraPath: extraPath,
+				} ),
 			new DuplicatePackageCheckerPlugin(),
 			shouldCheckForCycles &&
 				new CircularDependencyPlugin( {
