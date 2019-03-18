f89b53b260e3d3dffb1ff2843be20e782b734fd3
Bernie Reiter <ockham@raz.or.at>
2019-03-18 12:21:37 +0100

    Move Blocks specific babel config to jetpack-blocks

--- packages/calypso-build/babel.config.js
+++ packages/calypso-build/babel.config.js
@@ -39,27 +39,6 @@ const config = {
 			},
 		],
 	],
-	overrides: [
-		{
-			test: [ './client/gutenberg/extensions', './packages/jetpack-blocks' ],
-			plugins: [
-				[
-					'@wordpress/import-jsx-pragma',
-					{
-						scopeVariable: 'createElement',
-						source: '@wordpress/element',
-						isDefault: false,
-					},
-				],
-				[
-					'@babel/transform-react-jsx',
-					{
-						pragma: 'createElement',
-					},
-				],
-			],
-		},
-	],
 	env: {
 		build_pot: {
 			plugins: [
