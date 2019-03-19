8a7675f2c9116f219394b3f4d12bb274f1d58020
Bernie Reiter <ockham@raz.or.at>
2019-03-19 19:23:33 +0100

    Jetpack Blocks: Move contents of .babelrc.js to babel.config.js

--- packages/jetpack-blocks/babel.config.js
+++ packages/jetpack-blocks/babel.config.js
@@ -1,60 +1,19 @@
-const config = {
-	presets: [
-		[
-			'@babel/env',
-			{
-				targets: { browsers: [ 'last 2 versions', 'Safari >= 10', 'iOS >= 10', 'ie >= 11' ] },
-				useBuiltIns: 'entry',
-				// allows es7 features like Promise.prototype.finally
-				shippedProposals: true,
-				// Exclude transforms that make all code slower, see https://github.com/facebook/create-react-app/pull/5278
-				exclude: [ 'transform-typeof-symbol' ],
-			},
-		],
-		'@babel/react',
-	],
+module.exports = {
+	extends: require.resolve( '@automattic/calypso-build/babel.config.js' ),
 	plugins: [
-		'@babel/plugin-proposal-class-properties',
-		'@babel/plugin-proposal-export-default-from',
-		'@babel/plugin-proposal-export-namespace-from',
-		'@babel/plugin-syntax-dynamic-import',
 		[
-			'@babel/transform-runtime',
+			'@wordpress/import-jsx-pragma',
 			{
-				corejs: false, // we polyfill so we don't need core-js
-				helpers: true,
-				regenerator: false,
-				useESModules: false,
+				scopeVariable: 'createElement',
+				source: '@wordpress/element',
+				isDefault: false,
+			},
+		],
+		[
+			'@babel/transform-react-jsx',
+			{
+				pragma: 'createElement',
 			},
 		],
 	],
-	overrides: [
-		{
-			test: './src',
-			plugins: [
-				[
-					'@wordpress/import-jsx-pragma',
-					{
-						scopeVariable: 'createElement',
-						source: '@wordpress/element',
-						isDefault: false,
-					},
-				],
-				[
-					'@babel/transform-react-jsx',
-					{
-						pragma: 'createElement',
-					},
-				],
-			],
-		},
-	],
-	env: {
-		test: {
-			presets: [ [ '@babel/env', { targets: { node: 'current' } } ] ],
-			plugins: [ 'add-module-exports', 'babel-plugin-dynamic-import-node' ],
-		},
-	},
 };
-
-module.exports = config;
